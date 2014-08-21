5e1c1b941b2816f62fa9ae8c73be04d6a3945d8d
Marco Afonso <mafonso333@gmail.com>
2014-08-21 13:01:57 +0100

    Add demo comments

--- demo/client.js
+++ demo/client.js
@@ -1,27 +1,39 @@
 /**
- * User client
+ * Example Duality Client
  */
 
+// Do not call anything from outside!
 'use strict';
 
+// Create a client namespace
 var Client = {};
 
+// Define client routes configuration
 Client.routes = function (window, $) {
 
+	// Just call server at /example/json
 	$.getJSON('example/json', {get: 'test'}, function (data) {
+
+		// Check if there is a console
 		if (typeof console !== 'undefined') {
+
+			// Show server response in console
 			console.log(data);
 		}
 	});
-
 }
 
+// Define client initialization
 Client.init = function () {
 
+	// Check if we are on a browser environment
 	if (typeof window !== 'undefined' && typeof jQuery !== 'undefined') {
+
+		// Call jQuery on document ready
 		jQuery(function($) {
+
+			// Call client routes configuration
 			Client.routes(window, $);
 		});
 	}
-
 };
--- demo/index.php
+++ demo/index.php
@@ -7,32 +7,55 @@ spl_autoload_extensions('.php');
 spl_autoload_register('autoload');
 
 // What will we use in our application?
-use Duality\System\Structure\Http;
 use Duality\System\File\TextFile;
-use Duality\System\Server;
 use Duality\System\Structure\HtmlDoc;
-use Duality\System\Ajax;
+use Duality\System\Structure\Http;
+use Duality\System\Server;
 
-// Create a server, load request and create default response
+// Create a server with hostname and base URL
 $server = new Server('localhost', '/duality/demo');
+
+// Create an HttP request
 $request = new Http;
+
+// Parse request from globals
 $request->parseFromGlobals();
+
+// Create an HTTP response
 $response = $server->createResponse();
 
-// Configure server services with routes
+// Configure server with route /example/json
 $server->addRoute('/\/example\/json/i', function(&$request, &$response) use ($server) {
+
+	// Tell response to add HTTP content type header
 	$response->addHeader('Content-type', 'application/json');
+
+	// Tell response what is the output
 	$response->setContent(json_encode(array('result' => true, 'msg' => 'Ajax!')));
+
 });
 
 // Create default server response (an HTML document)
 $server->addRoute('/\//i', function(&$request, &$response) use ($server) {
+
+	// Create a new Document
 	$doc = new HtmlDoc();
+
+	// Create a template file by telling its path
 	$template = new TextFile('./demo/data/template.html');
+
+	// Tell to template to load its contents
 	$template->getContent();
+
+	// Tell Document to import from file
 	$doc->loadFile($template);
+
+	// Tell Document to append HTML to the container element
 	$doc->appendTo('//div[@id="container"]', '<h1 id="title">Hello Duality!</h1>');
+
+	// Tell response what is the output
 	$response->setContent($doc->save());
+	
 });
 
 // Finally, start server listen
